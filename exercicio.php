d534661e1f252610d083a164a6cbe2dff6955398
Thiago Moura <thiago.moura2000@hotmail.com>
2022-10-17 22:24:07 -0300

    Commit Inicial - Curso PHP - Elaboração de Estrutura para Exercícios

---
+++ exercicio.php
@@ -0,0 +1,33 @@
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    <title>Exercício</title>
+</head>
+
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem Formatação</a>
+        <a href="index.php" class="Vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            <?php
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php")
+                // include($_GET['dir'] . "/" . $_GET['file'] . ".php");
+            ?>
+        </div>
+    </main>
+    <footer class="rodape">
+        MOURA PHP - <?= date('Y'); ?>
+    </footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01 - Básico</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                    </ul>
+                </div>
+<!-- 
+                <div class="modulo vermelho">
+                    <h3>Módulo 02 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+
+                <div class="modulo azul">
+                    <h3>Módulo 03 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+
+                <div class="modulo roxo">
+                    <h3>Módulo 04 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+
+                <div class="modulo laranja">
+                    <h3>Módulo 05 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+
+                <div class="modulo verde-escuro">
+                    <h3>Módulo 06 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+
+                <div class="modulo vermelho-escuro">
+                    <h3>Módulo 07 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+
+                <div class="modulo azul-escuro">
+                    <h3>Módulo 08 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+
+
+                <div class="modulo roxo-escuro">
+                    <h3>Módulo 09 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div> -->
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        MOURA PHP - <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
